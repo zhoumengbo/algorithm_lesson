24affe1480322db4a75ed4d84c57eba2f0fb907d
zhoumengbo <zhoumengbo1106@163.com>
2022-04-27 16:28:36 +0800

    add test_2

--- final_test/test_2.py
+++ final_test/test_2.py
@@ -2,9 +2,40 @@
 # 使得A中的每一个点到A中另一个点都比到B中任意一个点近，反之亦然。
 # 请描述一个有效的算法能够做到这种划分。
 
+# input:
+# 第1行：M, 平面中的点的数量
+# 后面M行：x y, 分别是点的横纵坐标
+# example:
+# 5
+# 1 1
+# 1 3
+# 3 4
+# 4 4
+# 2 2
+
+import math
+
+
+def get_distance(point1, point2):
+    return math.sqrt(math.pow((point2[0] - point1[0]), 2) + math.pow((point2[1] - point1[1]), 2))
+
+
 if __name__ == '__main__':
-    key_list = [i + 1 for i in range(5)]
-    side_dict = {}.fromkeys(key_list, [])
-    print(side_dict)
-    side_dict[2] = [1, 2, 3]
-    print(side_dict)
+    M = int(input())
+    point_list = []
+    distance_list = [[] for _ in range(M)]
+    for _ in range(M):
+        point = list(map(int, input().split(' ')))
+        point_list.append(point)
+    print(point_list)
+    for i in range(len(point_list) - 1):
+        for j in range(i, len(point_list)):
+            distance = get_distance(point_list[i], point_list[j])
+            distance_list[i].append(distance)
+            if i != j:
+                distance_list[j].append(distance)
+    print(distance_list)
+
+
+
+
