6f0bd7f4753cd600470227b2abe71cc64819f9c1
zhoumengbo <zhoumengbo1106@163.com>
2022-05-28 16:00:33 +0800

    add test_3

--- final_test/test_3.py
+++ final_test/test_3.py
@@ -2,11 +2,29 @@
 # 假设现在有n个位置的接送请求，并且有n辆专车，其中第i辆车到位置j的距离dij是给定的。
 # 请你描述一个派遣n辆专车到n个地点的有效算法，使得n个专车的总行程最小。
 
-# 距离矩阵(n = 4)：
-distance = [[1, 3, 5, 7],
-            [2, 4, 6, 8],
-            [3, 6, 7, 9],
-            [5, 4, 3, 5]]
+import numpy as np
+from scipy.optimize import linear_sum_assignment
 
-if __name__ == '__main__':
-    print(distance)
+# input:
+# 第1行：n, 车和地点数量
+# 后面n行：第n辆车分别到各个地点的距离
+# example:
+# 4
+# 1 3 5 7
+# 2 4 6 8
+# 5 4 3 5
+# 3 6 7 10
+
+if __name__ == "__main__":
+    n = int(input())
+    cost = []
+    for i in range(n):
+        cost.append(list(map(int, input().split())))
+    cost = np.array(cost)
+    row_index, col_index = linear_sum_assignment(cost)
+    print(row_index)  # 开销矩阵对应的行索引
+    print(col_index)  # 对应行索引的最优指派的列索引
+    for i in row_index:
+        print("第{0}辆车应该去第{1}个地点，距离为{2}".format(i, col_index[i], cost[i, col_index[i]]))
+    print("最小总行程: {0}".format(cost[row_index, col_index].sum()))
+
